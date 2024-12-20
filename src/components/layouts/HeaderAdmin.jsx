007549de336c3321db6e23bbe34ffea756faae66
ann573 <ngthanhan0507@gmail.com>
2024-12-20 22:48:55 +0700

    eee

--- src/components/layouts/HeaderAdmin.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HeaderAdmin = () => {
-  return (
-    <div className='text-4xl font-bold mb-5'>Hello Admin</div>
-  )
-}
-
-export default HeaderAdmin
--- src/schema/product.js
+++
@@ -1,9 +0,0 @@
-import * as z from 'zod'
-
-const schemaProduct = z.object({
-    title: z.string().trim().min(6 , { message: "Yêu cầu tên tối thiểu 3 kí tự" }),
-    price: z.number().positive(),
-    description: z.string().optional()
-  });
-
-export default schemaProduct
